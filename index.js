cdbf7b5e28b76b35bfa4ca8344dbc87f9e0dedc4
Emanuel Ortega <emanuel.ortega@hotmail.com>
2023-09-14 00:08:04 -0600

    Missing fix delete and update item

--- index.js
+++ index.js
@@ -1,40 +1,80 @@
+const fs = require("fs")
+const util = require("util")
+
 class ProductManager{
-    constructor(){
-        this.products = [];
+    constructor(path){
+        this.path = path
     }
 
-    addProduct(title,description,product,price,thumbnail,code,stock){
-
-        const item = {
-            id: this.products.length + 1,
-            title: title,
-            description: description,
-            product: product,
-            price: price,
-            thumbnail: thumbnail,
-            code: code,
-            stock: stock
-        }
-        const found = this.products.find(product => product.code === code)
-        if(!found){
-            this.products.push(item);
+    async readProducts(path){
+        if(fs.existsSync(path)){
+            const db_file = await fs.promises.readFile(path,"utf-8")
+            return JSON.parse(db_file)
         }else{
-            console.log('El producto ya existe')
+            return []
         }
     }
 
-    getProductById(id){
-        const found = this.products.find(product => product.id === id)
-        if(found){
-            return found
+    async writeProducts(products){
+        try{
+            await fs.promises.writeFile(this.path,products)
+        }catch(err){
+            console.log("items couldn't be written. Please try again later")
+            console.log(err)
+        }
+    }
+
+    async addProduct(title,description,product,price,thumbnail,code,stock){   
+        try{
+            let products = await this.readProducts(this.path)
+            const item = {
+                id: products.length + 1,
+                title: title,
+                description: description,
+                product: product,
+                price: price,
+                thumbnail: thumbnail,
+                code: code,
+                stock: stock
+            }
+            const found = products.find(product => product.code === code)
+            console.log(found)
+            if(!found){
+                products.push(item);
+                await this.writeProducts(JSON.stringify(products))
+            }else{
+                console.log('El producto ya existe')
+            }
+        }catch(err){
+            console.log("Error en la BD. Contacta admin")
+        }
+    }
+
+    async getProducts(){
+        try{
+            let db = await this.readProducts(this.path)
+            return db
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    updateProduct(){}
+
+    async getProductById(id){
+        const products = await this.getProducts()
+        const found_obj = products.find(product => product.id === id)
+        if(found_obj){
+            return found_obj
         }else{
             console.log('Not found')
         }
     }
 }
 
-const newProduct = new ProductManager();
+const newProduct = new ProductManager("db.json");
 newProduct.addProduct("Arroz","Arroz de 1kg","Arroz","$100","https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cronista.com%2Fclase%2Fmercados%2FArroz-precios-estables-y-buenas-expectativas-para-la-cosecha-20210225-0001.html&psig=AOvVaw0Q4Z3X0Z3Z2Q4QX6Q4QX6Q&ust=1634178975213000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjQ4ZqH0_MCFQAAAAAdAAAAABAD","1234","10")
-
-console.log(newProduct.getProductById(1))
-console.log(newProduct.getProductById(2))
+//newProduct.getProducts()
+//newProduct.getProductById(1)
+//console.log(newProduct.getProductById(1))
+//console.log(newProduct.getProductById(2))
