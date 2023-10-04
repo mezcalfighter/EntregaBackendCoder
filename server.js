a16220d941cc51541f314313dfc3543beaba01f9
Emanuel Camarena <emanuel.ortega@hotmail.com>
2023-10-04 01:17:36 -0600

    Missing routers, manager complete

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 import express from "express"
 import productsRouter from "./src/routes/products.router.js"
+import cartRouter from "./src/routes/cart.router.js"
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/api/products",productsRouter)
-
-// CARRITO
-// api/carts
-// POST - Crear un carrito: idNumber, array productos
-// GET /:cid - Productos que pertenezcan al carrito
-// POST /:cid/product/:pid - solo debe contener el id del producto, es crucial no agregar el producto completo. Cantidad. El producto se agrega de uno en uno. Si el producto ya existe entonces es incremental el numero
-
+app.use("api/carts",cartRouter)
 
 app.listen(3000,()=>{
     console.log("Server is running @ http://localhost:3000")
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -2,6 +2,18 @@ import { Router } from "express"
 
 const router = Router()
 
+// POST - Crear un carrito: idNumber, array productos
+router.post("/",(req,res)=>{
 
+})
 
+// GET /:cid - Productos que pertenezcan al carrito
+router.get("/:id",(req,res)=>{
+
+})
+
+// POST /:cid/product/:pid - solo debe contener el id del producto, es crucial no agregar el producto completo. Cantidad. El producto se agrega de uno en uno. Si el producto ya existe entonces es incremental el numero
+router.post("/:id",(req,res)=>{
+    
+})
 export default router
